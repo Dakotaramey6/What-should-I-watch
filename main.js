c7ac4769eaeb0724ffac130c1bb85752572a350a
Dakota Ramey <71187507+Dakotaramey6@users.noreply.github.com>
2023-12-14 00:10:40 -0500

    Created the style of the site and added a restart

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 "use strict";
 const findMovieBtn = document.getElementById("movie-btn");
 const movieBody = document.getElementById("movie-res");
-let randomMovie = Math.floor(Math.random() * 600);
 
 //if you want to use this please request an api key from https://www.themoviedb.org/ AND make a var called let apiKey ='KeyID'
+let randomMovie = Math.floor(Math.random() * 600);
 let url = `https://api.themoviedb.org/3/movie/${randomMovie}?api_key=${apiKey}`;
 //when pushing to git, hide key in folder
 
-function createMovieElement(item, typeOfElement) {
+function createMovieElement(
+  item,
+  typeOfElement,
+  elID = `${item}-created`,
+  elClassName = "default"
+) {
   let createditem = document.createElement(typeOfElement);
   if (typeOfElement === "img") {
     createditem.src = `https://image.tmdb.org/t/p/w500/${item}`;
     createditem.alt = `Movie Cover`;
     createditem.id = `Movie-image`;
+    createditem.className = "img-rounded";
     movieBody.appendChild(createditem);
   } else {
     createditem.innerText = item;
@@ -20,8 +26,9 @@ function createMovieElement(item, typeOfElement) {
     if (createditem.innerText.length > 60) {
       createditem.id = `long-description`;
     } else {
-      createditem.id = `${item}-created`;
+      createditem.id = elID;
     }
+    createditem.className = elClassName;
     movieBody.appendChild(createditem);
   }
 }
@@ -33,19 +40,52 @@ function createNonMovieElement(text, typeOfElement) {
   movieBody.appendChild(textToDoc);
 }
 
+function collapseButton() {
+  const collapseBtn = document.createElement("button");
+  collapseBtn.dataset.toggle = "collapse";
+  collapseBtn.dataset.target = "#itemsToCollapse";
+  collapseBtn.innerText = "Genres";
+  movieBody.appendChild(collapseBtn);
+}
+
+function clearAll() {
+  movieBody.replaceChildren();
+
+  findMovieBtn.classList = "btn btn-primary btn-block active";
+}
+
+function startOver() {
+  const startOverbtn = document.createElement("button");
+  startOverbtn.className = "btn btn-primary btn-block";
+  startOverbtn.innerText = "Try again?";
+  movieBody.appendChild(startOverbtn);
+
+  startOverbtn.addEventListener("click", function () {
+    clearAll();
+    getMovie();
+  });
+}
+
 async function getMovie() {
   const res = await fetch(url);
   const movie = await res.json();
-  console.log(movie);
+
   createMovieElement(movie.poster_path, "img");
   createMovieElement(movie.original_title, "h2");
   createMovieElement(movie.overview, "p");
   if (movie.genres) {
-    createNonMovieElement("Genres:", "p");
+    collapseButton();
+    let collapseDiv = document.createElement("div");
+    collapseDiv.id = "itemsToCollapse";
+    collapseDiv.className = "collapse";
+    movieBody.appendChild(collapseDiv);
     movie.genres.forEach((element) => {
-      createMovieElement(element.name, "p");
+      collapseDiv.innerText += ` ${element.name}`;
+      // createMovieElement(element.name, "p");
     });
   }
+  findMovieBtn.classList = "btn btn-primary btn-block disabled";
+  startOver();
 }
 
-findMovieBtn.onclick = getMovie;
+findMovieBtn.addEventListener("click", getMovie, { once: true });
